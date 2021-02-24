a3f5f1f3b4c63eb21fdfc34784ec296a44f7108e
atsushi-tago-0209 <0209at@gmail.com>
2021-02-25 06:12:57 +0900

    最初の画面表示

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      max-height="60"
+    >
+    <v-toolbar-title>YouTube Curation</v-toolbar-title>
+    </v-toolbar>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+    <v-toolbar
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      max-height="60"
+    >
+    <v-spacer></v-spacer>
+      <p>@QuestAcademia</p>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
